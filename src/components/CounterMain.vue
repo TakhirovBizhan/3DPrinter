af47833b61fab168a329c3806571283ffe83e609
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-11-16 02:31:51 +0300

    build: installed every extenshion and library which i will use.

---
+++ src/components/CounterMain.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const count = ref(0);
+
+const inc = () => count.value++;
+
+</script>
+
+<template>
+  <el-button type="primary">Primary</el-button>
+  <button @click="inc">{{ count }}</button>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import CounterMain from '../components/CounterMain.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <CounterMain />
   </main>
 </template>
